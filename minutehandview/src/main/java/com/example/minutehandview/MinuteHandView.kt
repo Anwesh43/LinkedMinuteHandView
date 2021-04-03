cfc920341921ad6122f0cab1b754ec09293a3b00
Anwesh43 <anweshthecool0@gmail.com>
2021-04-04 03:44:41 +0530

    adding constants and libraries

--- minutehandview/src/main/java/com/example/minutehandview/MinuteHandView.kt
+++ minutehandview/src/main/java/com/example/minutehandview/MinuteHandView.kt
@@ -1,2 +1,19 @@
 package com.example.minutehandview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val foreColor : Int = Color.parseColor("#64DD17")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val strokeFactor : Float = 90f
+val arcFactor : Float = 4.9f
+val minuteFactor : Float = 7.8f
+val delay : Long = 20
+val deg : Float = 360f
+val hours : Int = 12
+val hourDeg : Float = deg / hours
