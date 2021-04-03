7359623ffbacce82f2b97ae40efab8330dc52d20
Anwesh43 <anweshthecool0@gmail.com>
2021-04-04 03:56:52 +0530

    adding MinuteHandView class

--- minutehandview/src/main/java/com/example/minutehandview/MinuteHandView.kt
+++ minutehandview/src/main/java/com/example/minutehandview/MinuteHandView.kt
@@ -69,3 +69,19 @@ fun Canvas.drawMHNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawMinuteHand(scale, w, h, paint)
 }
+
+class MinuteHandView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
