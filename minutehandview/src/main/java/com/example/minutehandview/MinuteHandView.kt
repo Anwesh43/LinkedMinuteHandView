9ac3c336d9042ce2decf5325314e4669ed3773fa
Anwesh43 <anweshthecool0@gmail.com>
2021-04-04 03:49:03 +0530

    adding Int and Float extension methods

--- minutehandview/src/main/java/com/example/minutehandview/MinuteHandView.kt
+++ minutehandview/src/main/java/com/example/minutehandview/MinuteHandView.kt
@@ -8,7 +8,16 @@ import android.graphics.Color
 import android.app.Activity
 import android.content.Context
 
-val foreColor : Int = Color.parseColor("#64DD17")
+val colors : Array<Int> = arrayOf(
+    "#64DD17",
+    "#f44336",
+    "#FF5722",
+    "#004D40",
+    "#2962FF"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+
 val backColor : Int = Color.parseColor("#BDBDBD")
 val strokeFactor : Float = 90f
 val arcFactor : Float = 4.9f
@@ -17,3 +26,10 @@ val delay : Long = 20
 val deg : Float = 360f
 val hours : Int = 12
 val hourDeg : Float = deg / hours
+val parts : Int = hours + 2
+val scGap : Float = 0.02f / parts
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n)
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
